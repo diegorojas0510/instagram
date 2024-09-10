3dd695adb2202d0554da36c03e9bbff395a6cb79
Diego Rojas <diego.rojas@symplifica.com>
2024-09-09 22:53:40 -0500

    Install mode Dark

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,22 +1,23 @@
 module ContentsHelper
   # metodo que recibe la fecha como argumento
   def date_format(date)
+    date.strftime("%d de %B de %Y")
     # listado de meses
-    meses = %w[Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre]
+    #meses = %w[Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre]
     # formato en español del mes extrayendo el mes de la fecha
-    mes = meses[date.strftime("%m").to_i - 1]
+    #mes = meses[date.strftime("%m").to_i - 1]
     # obtener el día
-    dia = date.strftime("%d")
+    #dia = date.strftime("%d")
     # obtener el año
-    year = date.strftime("%Y")
+    #year = date.strftime("%Y")
     # formato para la fecha
-    "#{dia} de #{mes} de #{year}"
+    #"#{dia} de #{mes} de #{year}"
   end
 
 
   # metodo que recibe como arguento la descripción de la tarjeta
   def simple_description(description)
-    # retorna los 100 primeros caracteres en la card para que no se dañe y lo puedo usar en todas las vistas
+    # retorna los 30 primeros caracteres en la card para que no se dañe y lo puedo usar en todas las vistas
     description.truncate(30)
   end
 
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Instagram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.time_zone = 'America/Bogota' # Ajusta a tu zona horaria
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
